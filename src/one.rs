7165117203961ce4e7357064b462c747b80c931d
Garry Michael Pereira <garrymp12@gmail.com>
2023-12-03 20:31:52 -0600

    finish day one part two

--- src/one.rs
+++ src/one.rs
@@ -1,19 +1,97 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn word_to_number(word: &str) -> Option<u8> {
-    match word {
-        "one" => Some(1),
-        "two" => Some(2),
-        "three" => Some(3),
-        "four" => Some(4),
-        "five" => Some(5),
-        "six" => Some(6),
-        "seven" => Some(7),
-        "eight" => Some(8),
-        "nine" => Some(9),
-        _ => None,
+#[allow(dead_code)]
+fn word_to_number(word: &str) -> Option<u64> {
+    let number_words = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    number_words.iter().find_map(
+        |(key, &num)| {
+            if word.contains(key) {
+                Some(num)
+            } else {
+                None
+            }
+        },
+    )
+}
+
+fn first_num(line: &str) -> (u64, isize) {
+    let (mut num, mut index) = (0, -1);
+    for (i, c) in line.chars().enumerate() {
+        if let Some(n) = c.to_digit(10) {
+            num = (n as u64) * 10;
+            index = i as isize;
+            break;
+        }
     }
+    (num, index)
+}
+
+fn last_num(line: &str) -> (u64, usize) {
+    let (mut num, mut index) = (0, 0);
+    let mut right = line.chars().count() - 1;
+    for _ in line.chars() {
+        if let Some(n) = line.chars().nth(right).unwrap().to_digit(10) {
+            num = n as u64;
+            index = right;
+            break;
+        } else if right > 0 {
+            right -= 1;
+        } else {
+            break;
+        }
+    }
+    (num, index)
+}
+
+#[allow(dead_code)]
+fn first_word(line: &str, slice: isize) -> Option<u64> {
+    let mut num: Option<u64> = None;
+    let l_slice: &str = if slice == -1 {
+        &line
+    } else {
+        &line[0..slice as usize]
+    };
+
+    let mut l_check: String = String::new();
+
+    for char in l_slice.chars() {
+        l_check.push(char);
+        if let Some(n) = word_to_number(&l_check) {
+            num = Some((n as u64) * 10);
+            break;
+        }
+    }
+    num
+}
+
+#[allow(dead_code)]
+fn last_word(line: &str, slice: usize) -> Option<u64> {
+    let mut num: Option<u64> = None;
+    let slice_len = line.chars().count();
+    let r_slice: &str = &line[slice + 1..slice_len];
+    let mut r_check: String = String::new();
+
+    for c in r_slice.chars().rev() {
+        r_check.insert(0, c);
+        if let Some(n) = word_to_number(&r_check) {
+            num = Some(n as u64);
+            break;
+        }
+    }
+    num
 }
 
 #[allow(dead_code)]
@@ -23,87 +101,17 @@ pub fn part_one(input: File) -> u64 {
     let reader = BufReader::new(input);
 
     for line in reader.lines() {
-        // indexes
-        let (mut left, mut right, line_len) = (
-            0 as usize,
-            line.as_ref().expect("no line").chars().count() - 1,
-            line.as_ref().expect("no line").chars().count(),
-        );
-        // switch flips on first occurence of number
-        let (mut r_check, mut l_check, mut no_num) = (true, true, false);
-        let (mut r_first_word, mut l_first_word) = (false, false);
-        // holds numbers when numbers are met, does not add them to sum yet
-        let (mut l_hold, mut r_hold) = (0, 0);
-        for char in line.as_ref().unwrap().chars() {
-            if l_check {
-                if let Some(n) = char.to_digit(10) {
-                    l_hold += (n as u64) * 10;
-                    l_check = false;
-                } else {
-                    left += 1;
-                }
-            }
-
-            if r_check {
-                if let Some(n) = line
-                    .as_ref()
-                    .unwrap()
-                    .chars()
-                    .nth(right)
-                    .unwrap()
-                    .to_digit(10)
-                {
-                    r_hold += n as u64;
-                    r_check = false;
-                } else {
-                    right -= 1;
-                }
-            }
-
-            if left == line_len && !no_num {
-                no_num = true;
-            }
-
-            if (!l_check && !r_check) || no_num {
-                // // iterate over the left slice
-                // let l_slice: &str = &line.as_ref().unwrap()[0..left];
-                // let mut l_check: String = String::new();
-
-                // for char in l_slice.chars() {
-                //     if char.is_alphabetic() {
-                //         l_check.push(char);
-                //         if let Some(n) = word_to_number(&l_check) {
-                //             sum += n as u64 * 10;
-                //             l_hold = 0;
-                //             break;
-                //         }
-                //     } else {
-                //         l_check.clear();
-                //     }
-                // }
-
-                // // iterate over right slice
-                // let r_slice: &str = &line.as_ref().unwrap()[(right + 1)..line_len];
-                // let mut r_check: String = String::new();
-
-                // for char in r_slice.chars() {
-                //     if char.is_alphabetic() {
-                //         r_check.push(char);
-                //         if let Some(n) = word_to_number(&r_check) {
-                //             sum += n as u64;
-                //             r_hold = 0;
-                //             break;
-                //         }
-                //     } else {
-                //         r_check.clear();
-                //     }
-                // }
-
-                sum += l_hold;
-                sum += r_hold;
-                break;
-            }
+        let line = line.unwrap();
+        let (mut f_num, f_num_index) = first_num(line.as_ref());
+        let (mut l_num, l_num_index) = last_num(line.as_ref());
+        if let Some(n) = first_word(line.as_ref(), f_num_index) {
+            f_num = n;
         }
+        if let Some(n) = last_word(line.as_ref(), l_num_index) {
+            l_num = n;
+        }
+        println!("{} and {}", f_num, l_num);
+        sum += f_num + l_num;
     }
     sum
 }
@@ -127,7 +135,7 @@ mod tests {
         let file: File = File::open("./src/real/one.txt").unwrap();
         let output: u64 = part_one(file);
         println!("answer to part one real: {}", output);
-        assert_eq!(output, 0);
+        assert_eq!(output, 54877);
     }
 
     #[test]
